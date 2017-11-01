8854681deb968c695d0f3bd112f05ea5a04d8f6c
Dennys Henry <daugmaud@gmail.com>
2017-11-01 21:04:25 +0300

    Designed Dashboard for Finance for SRs and WV/GF team

--- api/src/main/java/org/openmrs/module/mdrtbManagement/MdrtbManagementModuleActivator.java
+++ api/src/main/java/org/openmrs/module/mdrtbManagement/MdrtbManagementModuleActivator.java
@@ -11,7 +11,7 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-package org.openmrs.module.mdrtbManagement;
+package org.openmrs.module.mdrtbmanagement;
 
 
 import org.apache.commons.logging.Log; 
---
+++ omod/src/main/webapp/fragments/dashboardwvgf.gsp
@@ -0,0 +1,4 @@
+<%
+    ui.decorateWith("appui", "standardEmrPage", [title: "Finance Dashboard"])
+%>
+
